0aaf17f79ec5e14370eaf8a43feb9e8e7408c3c1
faizxdwm <faizxdwm@gmail.com>
2026-04-26 16:33:44 +0700

    update API

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnIzZQouW2ef4KMBMCfJ2-jeqZBn-htP0",
-  authDomain: "dicatat-app.firebaseapp.com",
-  projectId: "dicatat-app",
-  storageBucket: "dicatat-app.firebasestorage.app",
-  messagingSenderId: "986704563293",
-  appId: "1:986704563293:web:27c0dd0d45a370581991b4",
-  measurementId: "G-9MFP9XSDVN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
