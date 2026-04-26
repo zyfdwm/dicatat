3f6d51f3996413090198b2b8f19f268a31d13c8b
faizxdwm <faizxdwm@gmail.com>
2026-04-26 17:38:22 +0700

    fix

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,6 +3,7 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  // UploadThing v7 secara otomatis akan mencari UPLOADTHING_TOKEN di process.env
 });
 
 export const runtime = "edge";
