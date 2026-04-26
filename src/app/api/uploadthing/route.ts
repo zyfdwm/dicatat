0ca5698feac5d64470bb064119e591a8f3ea5935
faizxdwm <faizxdwm@gmail.com>
2026-04-26 18:05:24 +0700

    fix gatau keberapa

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,8 +1,26 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
+import { NextRequest } from "next/server";
 
-export const { GET, POST } = createRouteHandler({
+const handler = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    token: process.env.UPLOADTHING_TOKEN?.trim(),
+  },
 });
 
+export const POST = async (req: NextRequest) => {
+  try {
+    const res = await handler.POST(req);
+    return res;
+  } catch (e: any) {
+    return new Response(JSON.stringify({ 
+      error: "Server Error", 
+      message: e.message,
+      cause: e.cause 
+    }), { status: 500, headers: { "Content-Type": "application/json" } });
+  }
+};
+
+export const GET = handler.GET;
 export const runtime = "edge";
