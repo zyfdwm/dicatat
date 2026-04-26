84fb743df7bbed3e3ff90ae952655ad8f0c7f714
faizxdwm <faizxdwm@gmail.com>
2026-04-26 17:54:31 +0700

    fix lagi

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,9 +3,4 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    token: process.env.UPLOADTHING_TOKEN,
-  },
 });
-
-export const runtime = "edge";
